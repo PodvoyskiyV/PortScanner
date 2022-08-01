906375f5408ecf1ed6e47c1154b959d5616b05cf
Vadim Podvoyskiy <vadim.podvoyskiy@gmail.com>
2022-08-01 16:35:27 +0500

    First iteration

--- Tests.py
+++ Tests.py
@@ -2,3 +2,15 @@ import pyfiglet
 
 banner = pyfiglet.figlet_format("Heart")
 print(banner)
+
+Flag = True
+
+while(Flag):
+    x = int(input("Enter number of centimetres : "))
+
+    if x <= 0:
+        print("Try one more time")
+    else:
+        Flag = False
+
+print(x)
