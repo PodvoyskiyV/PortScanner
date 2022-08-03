986dfd1bb1d7d4967bdedbac901f0cb88ac06ca0
Vadim Podvoyskiy <vadim.podvoyskiy@gmail.com>
2022-08-03 17:58:39 +0500

    Protection from stupid people

--- main.py
+++ main.py
@@ -9,18 +9,53 @@ from datetime import datetime
 banner = pyfiglet.figlet_format("PORT SCANNER")
 print(banner)
 
-if len(sys.argv) >= 2:
-    target = socket.gethostbyname(sys.argv[1])
-else:
-    target = '192.168.0.100'
-    print("Invalid argument you must enter the IP like xxx.xxx.xxx.xxx as second parameter")
-
 try:
-    ports_number = sys.argv[2]
-    if ports_number > 65535 or ports_number < 0:
-        print('port must be 0-65535')
-except:
-    ports_number = 65535
+    if len(sys.argv) == 3:
+        target = socket.gethostbyname(sys.argv[1])
+        ports_number = sys.argv[2]
+    else:
+        print('Port Scanner will scan all ports from 0 to LAST')
+        ports_number = input('[*] Enter Last PORT: ')
+        while(True):
+            try:
+                if int(ports_number) > 65535 or int(ports_number) < 0:
+                    print('E: Last PORT is incorrect.')
+                    ports_number = input('[*] Enter Last PORT from 0 to 65535: ')
+                else:
+                    break
+            except ValueError:
+                print('E: Last PORT is incorrect.')
+                ports_number = input('[*] Enter Last PORT from 0 to 65535: ')
+
+        target = input('[*] Enter Target IP like xxx.xxx.xxx.xxx: ')
+
+    flag = True
+
+    while(flag):
+        target = (target.split(sep='.'))
+
+        if len(target) != 4:
+            print('E: Target IP is incorrect.')
+            target = input('[*] Enter Target IP like xxx.xxx.xxx.xxx where xxx is from 0 to 255: ')
+        else:
+            for quater in target:
+                try:
+                    quater = int(quater)
+                except ValueError:
+                    print('E: Target IP is incorrect.')
+                    target = input('[*] Enter Target IP like xxx.xxx.xxx.xxx where xxx is from 0 to 255: ')
+                    break
+
+                if quater < 0 or quater > 255:
+                    print('E: Target IP is incorrect.')
+                    target = input('[*] Enter Target IP like xxx.xxx.xxx.xxx where xxx is from 0 to 255: ')
+                    break
+            else:
+                flag = False
+                target = '.'.join(target)
+except KeyboardInterrupt:
+    print("\n Program finished !!!!")
+    sys.exit()
 
 print("-" * 50)
 print("Scanning Target: " + target)
@@ -28,7 +63,7 @@ print("Scanning started at:" + str(datetime.now()))
 print("-" * 50)
 
 try:
-    for port in range(0, ports_number + 1):
+    for port in range(0, int(ports_number) + 1):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         socket.setdefaulttimeout(1)
         result = s.connect_ex((target, port))
